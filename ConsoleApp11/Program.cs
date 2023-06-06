a52f219758032c9c047f27dcb451db23a3b6035e
vitor <vitor.hag@outlook.com>
2023-06-06 19:31:32 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,13 @@
+﻿double[] valores = new double[5];
+double soma = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Digite o {i + 1}° valor: ");
+    valores[i] = double.Parse(Console.ReadLine());
+    soma += valores[i];
+}
+
+double media = soma / 5;
+Console.WriteLine("A media é: " + media);
+  
